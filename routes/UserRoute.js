e4ba8f85dedc8694cd17d176480eb5d00fdca54a
Borhan <brborhan70@gmail.com>
2025-10-08 04:21:06 +0600

    validation simple and another use joi package

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,7 +1,11 @@
 const express = require("express");
-class user {
-  constructor(name, age) {
-    (name = this.name), (age = this.age);
+const Joi = require("joi");
+class User {
+  constructor(name, age, address, location) {
+    this.name = name;
+    this.age = age;
+    this.address = address;
+    this.location = location;
   }
 }
 const router = express.Router();
@@ -11,23 +15,42 @@ router.get("/users/:id", (req, res) => {
   res.send(`User Id: ${userId} , Filter   : ${filter}`);
 });
 
-router.post("/users", (req, res) => {
-  const users = user(req.body);
+router.post("/users/v1", (req, res) => {
+  const { name, age, address, location } = req.body;
 
-  if (!name) {
-    res.status(400).json({
-      error: "Name is required",
-    });
+  if (!name || !age || !address || !location) {
+    return res.status(400).json({ error: "All fields are required" });
   }
-  if (!age) {
-    res.status(400).json({
-      error: "Age is required",
-    });
-  }
-  const user = { name, age };
+
+  const user = new User(name, age, address, location);
   res.status(201).json({
     message: "User created successfully",
     user: user,
+  });
+});
+
+const userSchema = Joi.object({
+  name: Joi.string().min(3).max(50).required(),
+  age: Joi.number().positive().required(),
+  address: Joi.string().min(5).required(),
+  name: Joi.string().required(),
+});
+router.post("/users/v2", (req, res) => {
+  const { error } = userSchema.validate(req.bady);
+
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
+
+  const { name, age, address, location } = req.body;
+  res.status(201).json({
+    message: "User created successfully",
+    user: {
+      name,
+      age,
+      address,
+      location,
+    },
   });
 });
 
